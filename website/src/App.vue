19c675fa2a3bc80be0ff3efa7eab361774d6824f
Maria Lamardo <mlama007@github.com>
2026-09-16 11:25:04 -0400

    Move focus to top of page when navigating to new page

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,13 +1,30 @@
 <script setup lang="ts">
-import { RouterView } from "vue-router";
+import { nextTick, ref, watch } from "vue";
+import { RouterView, useRoute } from "vue-router";
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
+
+const route = useRoute();
+const mainContent = ref<HTMLElement>();
+
+watch(
+  () => route.path,
+  async () => {
+    if (route.hash) {
+      return;
+    }
+
+    await nextTick();
+    window.scrollTo({ top: 0, left: 0 });
+    mainContent.value?.focus({ preventScroll: true });
+  },
+);
 </script>
 
 <template>
   <a class="skip-link" href="#main">Skip to main content</a>
   <Header />
-  <main id="main" class="app-main" tabindex="-1">
+  <main id="main" ref="mainContent" class="app-main" tabindex="-1">
     <RouterView />
   </main>
   <Footer />
